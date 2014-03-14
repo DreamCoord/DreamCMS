2e8780a1a653d101edd69f3d4949261ed45e4a0f
Mark Ma <gbstack08@gmail.com>
2014-03-14 15:40:51 +0800

    add Page add, edit, delete

--- app/controllers/admin/Page.java
+++ app/controllers/admin/Page.java
@@ -1,6 +1,7 @@
 package controllers.admin;
 
 import java.text.DateFormat;
+import java.text.SimpleDateFormat;
 import java.util.Date;
 import java.util.List;
 
@@ -10,23 +11,45 @@ import models.mag_page;
 import play.mvc.Controller;
 
 public class Page extends Controller{
-	public static void addPage(String titleStr,String contentStr){
-		System.out.println(titleStr+contentStr);
-		DateFormat dt=DateFormat.getInstance();
-		String createtime = dt.format(new Date()).toString(); 
-		new mag_page(titleStr,contentStr,createtime,"admin").save();
-		renderJSON("ok");
+	public static void add(){
+		if(request.method=="GET"){
+			render();
+		}else{
+			mag_page page = new mag_page();
+			page.title = params.get("title");
+			page.content = params.get("content");
+			page.createtime = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss").format(new Date());
+			page.author =  session.get("username");
+			page.save();
+			
+			flash("msg", "<div class='alert alert-success'>添加成功</div>");
+			redirect("/admin/page");
+		}
 	}
 	public static void allPages(){
-		List<mag_page> pagelist = mag_page.find("order by id desc").fetch();
-		String jsonStr = JSON.toJSONString(pagelist);
-		String backJSON = "{\"pagelist\":"+jsonStr+"}";
-		renderJSON(backJSON);
-	}
-	public static void getPage(){
+		List<mag_page> pages = mag_page.find("order by id desc").fetch();
 		
+		String msg = flash.get("msg");
+		render(pages, msg);
 	}
-	public static void delPage(){
+	public static void edit(int id){
+		mag_page page = mag_page.findById(id);
+		if(request.method=="GET"){
+			render(page);
+		}else{
+			page.title = params.get("title");
+			page.content = params.get("content");
+			page.save();
+			
+			flash("msg", "<div class='alert alert-success'>修改成功</div>");
+			redirect("/admin/page");
+		}
+	}
+	public static void delete(int id){
+		mag_page page = mag_page.findById(id);
+		page.delete();
 		
+		flash("msg", "<div class='alert alert-success'>删除成功</div>");
+		redirect("/admin/page");
 	}
 }
