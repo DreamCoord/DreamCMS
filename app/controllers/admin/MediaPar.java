f27928f6287b0a82c166ad483a351d36ac910518
gbstack <gbstack08@gmail.com>
2014-03-25 15:43:19 +0800

    add filebrowserUploadUrl for CKEditor fixed image uploading issue for Article, Page adding/editing

--- app/controllers/admin/MediaPar.java
+++ app/controllers/admin/MediaPar.java
@@ -14,7 +14,7 @@ import play.mvc.Controller;
 import com.alibaba.fastjson.JSON;
 
 public class MediaPar extends Controller{
-	public static void addMediaPar(String title,String websiteURL,File image,String remark ){
+	public static void addMediaPar(File upload){
 		if(request.method == "GET"){
 			render();
 		}else{
@@ -22,7 +22,7 @@ public class MediaPar extends Controller{
 			String savePath =  Play.applicationPath.toString()+Play.configuration.getProperty("newsImg.savePath", "/public/upload/");
 			//文件保存目录URL
 			String saveUrl  =  Play.configuration.getProperty("newsImg.savePath", "/public/upload/");			
-			if (image != null) {
+			if (upload != null) {
 				//检查目录
 				File uploadDir = new File(savePath);
 				if(!uploadDir.isDirectory()){
@@ -41,19 +41,22 @@ public class MediaPar extends Controller{
 					dirFile.mkdirs();
 				}			
 				//检查扩展名
-				String fileExt = image.getName().substring(image.getName().lastIndexOf(".") + 1).toLowerCase();
+				String fileExt = upload.getName().substring(upload.getName().lastIndexOf(".") + 1).toLowerCase();
 				SimpleDateFormat df = new SimpleDateFormat("yyyyMMddHHmmss");
 				String newFileName = df.format(new Date()) + "_" + new Random().nextInt(1000) + "." + fileExt;
 				File f = new File(savePath,newFileName);
 				try {
-					Files.copy(image,f);  
+					Files.copy(upload,f);  
 					System.out.println(saveUrl + newFileName);
 					java.text.DateFormat format1 = new java.text.SimpleDateFormat(  
 			                "yyyy-MM-dd hh:mm");  
 					String timeStr = format1.format(new Date()).toString();		
 	
-					mag_mediapar banner = new mag_mediapar( title,  saveUrl + newFileName,websiteURL,remark, timeStr,"");
-					banner.save();
+					mag_mediapar media = new mag_mediapar();
+					media.title = upload.getName();
+					media.mappath = saveUrl+newFileName;
+					media.time = timeStr;
+					media.save();
 					
 					
 					flash("msg", "<div class='alert alert-success'>保存成功</div>");
@@ -68,6 +71,61 @@ public class MediaPar extends Controller{
 			allMediaPar();
 		}
 	}
+	
+	public static void ajaxAdd(File upload, String CKEditorFuncNum){
+		//文件保存目录路径
+		String savePath =  Play.applicationPath.toString()+Play.configuration.getProperty("newsImg.savePath", "/public/upload/");
+		//文件保存目录URL
+		String saveUrl  =  Play.configuration.getProperty("newsImg.savePath", "/public/upload/");			
+		if (upload != null) {
+			//检查目录
+			File uploadDir = new File(savePath);
+			if(!uploadDir.isDirectory()){
+				uploadDir.mkdirs();
+			}
+			//检查目录写权限
+			if(!uploadDir.canWrite()){
+				renderJSON("{\"state\":\"上传目录没有写权限。\"}");
+				return;
+			}
+			String ymd = "MediaPar";
+			savePath += ymd + "/";
+			saveUrl += ymd + "/";
+			File dirFile = new File(savePath);
+			if (!dirFile.exists()) {
+				dirFile.mkdirs();
+			}			
+			//检查扩展名
+			String fileExt = upload.getName().substring(upload.getName().lastIndexOf(".") + 1).toLowerCase();
+			SimpleDateFormat df = new SimpleDateFormat("yyyyMMddHHmmss");
+			String newFileName = df.format(new Date()) + "_" + new Random().nextInt(1000) + "." + fileExt;
+			File f = new File(savePath,newFileName);
+			try {
+				Files.copy(upload,f);  
+				System.out.println(saveUrl + newFileName);
+				java.text.DateFormat format1 = new java.text.SimpleDateFormat(  
+		                "yyyy-MM-dd hh:mm");  
+				String timeStr = format1.format(new Date()).toString();		
+
+				mag_mediapar media = new mag_mediapar();
+				media.title = upload.getName();
+				media.mappath = saveUrl+newFileName;
+				media.time = timeStr;
+				media.save();
+				
+				String media_url = saveUrl+newFileName;
+				render(CKEditorFuncNum, media_url);
+			} catch (Exception e) {
+				e.printStackTrace();
+				renderJSON("{\"state\":\"上传失败\"}");
+			}
+		}else{
+			renderJSON("{\"state\":\"请选择文件。\"}");
+		}
+		
+		render(CKEditorFuncNum, "");
+	}
+	
 	public static void allMediaPar(){
 		List<mag_mediapar> medias = mag_mediapar.find("order by id desc").fetch();
 		
